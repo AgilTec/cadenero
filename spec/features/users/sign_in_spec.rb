e64a1841742094d9d91b0782062a3e6468afc3c3
Manuel Vidaurre <manuel.vidaurre@agiltec.com.mx>
2013-07-12 10:39:59 -0500

    Extracting in a helper common RSpec helpers authentication methods for Cadenero

--- spec/features/users/sign_in_spec.rb
+++ spec/features/users/sign_in_spec.rb
@@ -1,45 +1,11 @@
 require 'spec_helper'
 require 'cadenero/testing_support/subdomain_helpers'
+require 'cadenero/testing_support/authentication_helpers'
 
 feature 'User sign in' do
   extend Cadenero::TestingSupport::SubdomainHelpers
-
-  def create_account_user
-    @user ||= { email: "user@example.com", password: "password", password_confirmation: "password" }
-  end
-
-  def account_user(user)
-    @user = { email: user.email, password: "password" }
-  end
-
-  def find_account_by_email
-    create_account_user
-    @account = Cadenero::V1::Account.where(name: @user[:email]).first
-  end
-
-  def sign_in_user(url, user)
-    post "#{url}", format: :json, user: user
-    find_account_by_email
-  end
-
-  def expected_json_errors(msg)
-    expect(last_response.body).to eql(msg)
-    expect(last_response.status).to eq 422    
-  end
-
-  def check_error_for_not_signed_in_yet
-    get cadenero.v1_root_url(:subdomain => account.subdomain)
-    expected_json_errors(errors_redirect_ro_sign_in)
-  end
-
-  def successful_sign_in_owner(account)
-    sign_in_user sessions_url, account_user(account.owner)
-    expect(last_response.status).to eq 201
-    expect(JSON.parse(last_response.body)["user"]["account_ids"]).to eq [account.id]
-    expect(JSON.parse(last_response.body)).to have_content "auth_token"
-    expect(JSON.parse(last_response.body)["user"]["auth_token"]).to eq account.authentication_token
-    return JSON.parse(last_response.body)["user"]["email"]
-  end
+  include Cadenero::TestingSupport::AuthenticationHelpers
+ 
 
   let(:account) { FactoryGirl.create(:account_with_schema) }
   let(:errors_redirect_ro_sign_in) {{errors: %Q{Please sign in. posting the user json credentials as: {"user": {"email": "testy2@example.com", "password": "changeme"}} to /v1/sessions}, links: "/v1/sessions"}.to_json}
--- spec/features/users/sign_up_spec.rb
+++ spec/features/users/sign_up_spec.rb
@@ -1,20 +1,9 @@
 require 'spec_helper'
-
-def create_account_user
-  @user ||= { email: "user@example.com", password: "password", password_confirmation: "password" }
-end
-
-def find_account_by_email
-  @account = Cadenero::V1::Account.where(name: @user[:email]).first
-end
-
-def sign_up_user(url)
-  create_account_user
-  post "#{url}/v1/users", format: :json, user: @user
-  find_account_by_email
-end
+require 'cadenero/testing_support/authentication_helpers'
 
 feature "User signup" do
+  include Cadenero::TestingSupport::AuthenticationHelpers
+
   let!(:account) { FactoryGirl.create(:account_with_schema) }
   let(:root_url) { "http://#{account.subdomain}.example.com/" }
   scenario "under an account" do
