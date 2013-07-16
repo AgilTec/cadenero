63ca9a77f705414051041dbefc250f8c6cf6d453
Manuel Vidaurre <manuel.vidaurre@agiltec.com.mx>
2013-07-15 19:44:44 -0500

    Updating seeds to associate Account and User by Member

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,5 @@
-@account = Cadenero::V1::Account.create!(name: Cadenero.default_account_name,
+@account = Cadenero::V1::Account.create_with_owner(name: Cadenero.default_account_name,
                               subdomain: Cadenero.default_account_subdomain,
                               owner: Cadenero::User.create!(email: Cadenero.default_user_email,
                                                             password: Cadenero.default_user_password,
                                                             password_confirmation: Cadenero.default_user_password))
-
-@account.create_schema
-@account.ensure_authentication_token!
